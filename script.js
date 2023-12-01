1b5cb4c8401cfc5de6c57376ee26163ae8223060
Ritika <116043234+ritikazahira@users.noreply.github.com>
2023-11-30 19:34:20 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const menu = document.querySelector('.menu');
+  menu.addEventListener('click', function() {
+    // Code to toggle menu visibility
+    // You'll need to implement this part based on how your menu is structured
+    console.log('Menu toggle clicked');
+  });
+});
