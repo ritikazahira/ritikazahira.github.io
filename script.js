c4628205b16c187a03968db59a3acbe7b4b8de09
Ritika <116043234+ritikazahira@users.noreply.github.com>
2023-12-01 21:20:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Existing smooth scroll functionality
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -5,4 +6,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             behavior: 'smooth'
         });
     });
+});
+
+// New scroll event listener to fade out the title
+document.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var titleOpacity = 1 - (scrollPosition / 500); // The rate of fading. Adjust the 500 to control the fade speed
+    var titleElement = document.querySelector('.centered-title h1');
+    if (titleElement) {
+        titleElement.style.opacity = titleOpacity > 0 ? titleOpacity : 0;
+    }
 });
